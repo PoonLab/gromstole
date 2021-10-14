4a1509b13c96cf33f34678f3e2c660e8e7d9d41c
Art Poon <artpoon@gmail.com>
2021-10-13 23:19:32 -0400

    hack to deal with paired-end read merging

--- scripts/minimap2.py
+++ scripts/minimap2.py
@@ -91,6 +91,57 @@ def minimap2(infile, ref, stream=False, path='minimap2', nthread=3, minlen=29000
         yield qname, rpos, cigar, seq
 
 
+def minimap2_paired(fq1, fq2, ref, path='minimap2', nthread=3):
+    """
+
+    :param fq1:  str, path to FASTQ R1 file (uncompressed or gzipped)
+    :param fq2:  str, path to FASTQ R2 file (uncompressed or gzipped)
+    :param ref:  str, path to FASTA file with reference sequence
+    :param path:  str, path to minimap2 binary executable
+    :param nthread:  int, number of threads to run
+    :return:
+    """
+    p = subprocess.Popen(
+        [path, '-t', str(nthread), '-ax', 'sr', '--eqx', ref, fq1, fq2],
+        stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
+    )
+    output = map(lambda x: x.decode('utf-8'), p.stdout)
+    for line in output:
+        if line == '' or line.startswith('@'):
+            # split on \n leaves empty line; @ prefix header lines
+            continue
+
+        qname, flag, rname, rpos, _, cigar, _, _, _, seq = \
+            line.strip('\n').split('\t')[:10]
+
+        if rname == '*' or ((int(flag) & 0x800) != 0):
+            # did not map, or supplementary alignment
+            continue
+
+        rpos = int(rpos) - 1  # convert to 0-index
+        yield qname, rpos, cigar, seq
+
+
+def matchmaker(samfile):
+    """
+    An iterator that returns pairs of reads sharing a common qname from a SAM file.
+    Note that unpaired reads will be left in the cached_rows dictionary and
+    discarded.
+    @param samfile: open file handle to a SAM file
+    @return: yields a tuple for each read pair with fields split by tab chars:
+        ([qname, flag, rname, ...], [qname, flag, rname, ...])
+    """
+    cached_rows = {}
+    for row in samfile:
+        qname = row[0]
+        old_row = cached_rows.pop(qname, None)
+        if old_row is None:
+            cached_rows[qname] = row
+        else:
+            # current row should be the second read of the pair
+            yield old_row, row
+
+
 # return aligned sequence?
 def output_fasta(iter, outfile, reflen=0):
     """
@@ -152,7 +203,7 @@ def stream_fasta(iter, reflen=0):
         yield qname, aligned
 
 
-def encode_diffs(iter, reflen, alphabet='ACGT'):
+def encode_diffs(qname, rpos, cigar, seq, reflen=29903, alphabet='ACGT'):
     """
     Serialize differences of query sequences to reference
     genome, which comprise nucleotide substitutions, in-frame
@@ -162,61 +213,60 @@ def encode_diffs(iter, reflen, alphabet='ACGT'):
     :param iter:  generator from minimap2()
     :param reflen:  length of reference genome
     """
-    for qname, rpos, cigar, seq in iter:
-        diffs = []
-        missing = []
-        if rpos > 0:
-            # incomplete on left
-            missing.append(tuple([0, rpos]))
-        left = 0  # index for query
+    diffs = []
+    missing = []
+    if rpos > 0:
+        # incomplete on left
+        missing.append(tuple([0, rpos]))
+    left = 0  # index for query
 
-        tokens = re.findall(r'  (\d+)([MIDNSHPX=])', cigar, re.VERBOSE)
-        for length, operator in tokens:
-            length = int(length)
-            substr = seq[left:(left + length)]
-            if operator == 'X':
-                # each nucleotide is a separate diff
-                if 'N' in substr:
-                    # for now, assume the whole substring is bs
-                    missing.append(tuple([rpos, rpos+length]))
-                else:
-                    # assume adjacent mismatches are independent substitutions
-                    for i, nt in enumerate(substr):
-                        if nt in alphabet:
-                            diffs.append(tuple(['~', rpos + i, nt]))
-                        else:
-                            # skip ambiguous base calls, like "R"
-                            missing.append(tuple([rpos+i, rpos+i+1]))
-
-                left += length
-                rpos += length
-            elif operator == 'S':
-                # discard soft clip
-                left += length
-            elif operator == 'I':
-                # insertion relative to reference
-                diffs.append(tuple(['+', rpos, substr]))
-                left += length
-            elif operator == 'D':
-                # deletion relative to reference
-                diffs.append(tuple(['-', rpos, length]))
-                rpos += length
-            elif operator == '=':
-                # exact match
-                left += length
-                rpos += length
-            elif operator == 'H':
-                # hard clip, do nothing
-                pass
+    tokens = re.findall(r'  (\d+)([MIDNSHPX=])', cigar, re.VERBOSE)
+    for length, operator in tokens:
+        length = int(length)
+        substr = seq[left:(left + length)]
+        if operator == 'X':
+            # each nucleotide is a separate diff
+            if 'N' in substr:
+                # for now, assume the whole substring is bs
+                missing.append(tuple([rpos, rpos+length]))
             else:
-                print("ERROR: unexpected operator {}".format(operator))
-                sys.exit()
+                # assume adjacent mismatches are independent substitutions
+                for i, nt in enumerate(substr):
+                    if nt in alphabet:
+                        diffs.append(tuple(['~', rpos + i, nt]))
+                    else:
+                        # skip ambiguous base calls, like "R"
+                        missing.append(tuple([rpos+i, rpos+i+1]))
 
-        # update missing if sequence incomplete on the right
-        if rpos < reflen:
-            missing.append(tuple([rpos, reflen]))
+            left += length
+            rpos += length
+        elif operator == 'S':
+            # discard soft clip
+            left += length
+        elif operator == 'I':
+            # insertion relative to reference
+            diffs.append(tuple(['+', rpos, substr]))
+            left += length
+        elif operator == 'D':
+            # deletion relative to reference
+            diffs.append(tuple(['-', rpos, length]))
+            rpos += length
+        elif operator == '=':
+            # exact match
+            left += length
+            rpos += length
+        elif operator == 'H':
+            # hard clip, do nothing
+            pass
+        else:
+            print("ERROR: unexpected operator {}".format(operator))
+            sys.exit()
 
-        yield qname, diffs, missing
+    # update missing if sequence incomplete on the right
+    if rpos < reflen:
+        missing.append(tuple([rpos, reflen]))
+
+    return qname, diffs, missing
 
 
 def extract_features(batcher, ref_file, binpath='minimap2', nthread=3, minlen=29000):
@@ -248,32 +298,26 @@ def extract_features(batcher, ref_file, binpath='minimap2', nthread=3, minlen=29
 
 def parse_args():
     parser = argparse.ArgumentParser("Wrapper script for minimap2")
-    parser.add_argument('infile', type=argparse.FileType('r'),
-                        help="<input> path to query FASTA file or database (--stream)")
+
+    parser.add_argument('fq1', type=str, help="<input> path to FASTQ R1 file.  May be gzip'ed.")
+    parser.add_argument('fq2', type=str, help="<input> path to FASTQ R2 file.  May be gzip'ed.")
     parser.add_argument('-o', '--outfile',
                         type=argparse.FileType('w'),
                         required=False,
                         help="<output, optional> path to write output, "
                              "defaults to stdout")
+    parser.add_argument('--path', type=str, default='minimap2', help="<option> path to minimap2 executable")
+
     parser.add_argument('-a', '--align', action='store_true',
                         help="<option> output aligned sequences as FASTA")
     parser.add_argument('-t', '--thread', type=int, default=3, 
                         help="<option> number of threads")
-    parser.add_argument('-f', '--force-headers', action='store_true',
-                        help="<option> use -f to force this script to accept "
-                             "headers with spaces, which will be truncated "
-                             "by minimap2")
+
     parser.add_argument('--filter', action='store_true',
                         help="<option> filter problematic sites")
 
-    path = covizu.__path__[0]
-    if '.egg' in path:
-        import pkg_resources
-        ref_file = pkg_resources.resource_filename('covizu', 'data/NC_045512.fa')
-        vcf_file = pkg_resources.resource_filename('covizu', "data/problematic_sites_sarsCov2.vcf")
-    else:
-        ref_file = os.path.join(path, "data", "NC_045512.fa")
-        vcf_file = os.path.join(path, "data", "problematic_sites_sarsCov2.vcf")
+    ref_file = os.path.join("data", "NC_045512.fa")
+    vcf_file = os.path.join("data", "problematic_sites_sarsCov2.vcf")
 
     parser.add_argument('--ref', type=str, default=ref_file,
                         help="<input> path to target FASTA (reference)")
@@ -281,35 +325,28 @@ def parse_args():
                         help="Path to VCF file of problematic sites in SARS-COV-2 genome. "
                              "Source: https://github.com/W-L/ProblematicSites_SARS-CoV2")
 
-    parser.add_argument('--minlen', help="<option> minimum sequence length, "
-                                         "defaults to 29000nt.",
-                        type=int, default=29000)
     return parser.parse_args()
 
 
 if __name__ == '__main__':
-    from seq_utils import load_vcf, apply_features
     args = parse_args()
     if args.outfile is None:
         args.outfile = sys.stdout
 
-    # check input headers for spaces
-    if not args.force_headers:
-        for line in args.infile:
-            if line.startswith('>') and ' ' in line:
-                print("WARNING: at least one FASTA header contains a space")
-                print(line)
-                print("Use '-f' to force this script to process the file")
-                print("Otherwise use `sed -i 's/ /_/g' <file>` to replace all spaces in place.")
-                sys.exit()
+    mm2 = minimap2_paired(args.fq1, args.fq2, ref=args.ref, nthread=args.thread, path=args.path)
+    for pairs in matchmaker(mm2):
+        for qname, rpos, cigar, seq in pairs:
+            diffs = encode_diffs(qname, rpos, cigar, seq)
+            print(diffs)
+        break
 
-    mm2 = minimap2(args.infile, ref=args.ref, nthread=args.thread,
-                   minlen=args.minlen)
+    sys.exit()
 
     # get length of reference
     refseq = convert_fasta(open(args.ref))[0][1]
     reflen = len(refseq)
-    if args.align:
+    """
+        if args.align:
         if args.filter:
             vcf = load_vcf(args.vcf)
             encoded = encode_diffs(mm2, reflen=reflen)
@@ -318,11 +355,13 @@ if __name__ == '__main__':
                 args.outfile.write(">{}\n{}\n".format(qname, seq))
         else:
             output_fasta(mm2, reflen=reflen, outfile=args.outfile)
-    else:
-        # serialize feature vectors as JSON
-        res = []
-        for qname, diffs, missing in encode_diffs(mm2, reflen=reflen):
-            res.append({'name': qname, 'diffs': diffs, 'missing': missing})
-        serial = json.dumps(res).replace('},', '},\n')
-        args.outfile.write(serial)
+    """
+
+    # serialize feature vectors as JSON
+    res = []
+    for row in mm2:
+        qname, diffs, missing = encode_diffs(row)
+        res.append({'name': qname, 'diffs': diffs, 'missing': missing})
+    serial = json.dumps(res).replace('},', '},\n')
+    args.outfile.write(serial)
 
