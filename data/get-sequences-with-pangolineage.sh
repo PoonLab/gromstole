c007fd2e6d138d41b340e7adb03f6624e71abd77
DBecker7 <d7becker@gmail.com>
2021-10-19 16:20:22 -0400

    Only conisider sequences with known pangolineage.

---
+++ data/get-sequences-with-pangolineage.sh
@@ -0,0 +1,19 @@
+# Get data
+# cd data
+# ./download.sh
+
+# Unzip (-k means keep original)
+# Unfortunately, awk and seqtk subseq both need unzipped files
+unxz -k sequences.fasta.xz 
+gunzip -k metadata.tsv.gz
+
+# Get correct rows 
+# (column 20 is pangolineage, 1 is fasta description, row 1 is column headers)
+awk '{ if ($20 != "?") { print $1} }' metadata.tsv | awk 'NR!=1' > metadata_pango-names.txt
+
+# Find seqs
+seqtk subseq sequences.fasta metadata_pango-names.txt > sequences_pangolin.fasta
+
+# Clean up
+xz sequences_pangolin.fasta
+rm data/sequences.fasta
