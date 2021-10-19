8e149c94adbb27f03efb7491560968d8b8accbae
Art Poon <artpoon@gmail.com>
2021-10-19 10:29:20 -0400

    R script

---
+++ scripts/parse-vcf.R
@@ -0,0 +1,74 @@
+setwd('~/git/wastewater')
+
+# load VCF from Virontus on Landgraff Nanopore data
+longshot <- read.table("data/results2/vcf/SRR13912812-MN908947.3.longshot.vcf")
+medaka <- read.table("data/results2/vcf/SRR13912812-MN908947.3.medaka.vcf")
+
+# CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	SAMPLE
+names(longshot) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
+                     'info', 'format', 'sample')
+names(medaka) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
+                     'info', 'format', 'sample')
+
+
+longshot$depth <- sapply(longshot$info, function(x) {
+  tokens <- strsplit(x, ";")[[1]]
+  dp <- as.integer(strsplit(tokens[1], "=")[[1]][2])
+})
+
+longshot$count <- sapply(longshot$info, function(x) {
+  tokens <- strsplit(x, ";")[[1]]
+  sum(as.integer(strsplit(strsplit(tokens[2], "=")[[1]][2], ",")[[1]]))
+})
+
+# note these are substitutions only
+longshot$rel.freq <- longshot$count / longshot$depth
+
+longshot$label <- paste("~", longshot$pos, substr(longshot$alt, 1, 1), sep="")
+
+
+index <- match(longshot$pos, medaka$pos)
+longshot$medaka.q <- medaka$qual[index]
+
+# load our results for Illumina data from same study
+ont <- read.csv('data/ontario.csv')
+
+index <- match(longshot$label, ont$label)
+
+longshot$illumina <- ont$frequency[index]
+
+png(file='temp.png', width=5*150, height=5*150, res=150)
+par(mar=c(5,5,1,1))
+plot(longshot$rel.freq, longshot$illumina, xlim=c(0,1), ylim=c(0,1), type='n')
+text(longshot$rel.freq, longshot$illumina, label=longshot$label, cex=0.5)
+abline(a=0, b=1)
+dev.off()
+
+tab1 <- read.csv('data/landgraff-table1.csv', header=T)
+index <- match(tab1$label, longshot$label)
+
+
+# Our try at Nanopore
+nano <- read.csv('~/git/wastewater/data/SRR13912812.mapped.csv')
+
+# coverage plog
+temp <- cbind(nano$position, nano$coverage)
+temp <- temp[!duplicated(temp), ]
+temp <- temp[order(temp[,1]), ]
+png("coverage2.png", width=6*150, height=5*150, res=150)
+plot(temp[,1], temp[,2], type='s', log='y', xlab='Reference position', ylab='Read depth')
+dev.off()
+
+index <- match(ont$label, nano$label)
+ont$nanopore <- nano$frequency[index]
+
+foo <- ont[ont$coverage > 100, ]
+plot(foo$frequency, foo$nanopore)
+
+
+index <- match(longshot$label, nano$label)
+longshot$our.nano <- nano$frequency[index]
+
+plot(longshot$rel.freq, longshot$our.nano, type='n',
+     xlab='Virontus frequency', ylab='Our estimates on same ONT data')
+text(longshot$rel.freq, longshot$our.nano, label=longshot$label, cex=0.6)
