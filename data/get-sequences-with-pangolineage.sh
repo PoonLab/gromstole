54ab5826636c5b9813b294e88d8825f08df92347
DBecker7 <d7becker@gmail.com>
2021-10-19 16:22:53 -0400

    Proper clean up in pangolineage fasta subsetting

--- data/get-sequences-with-pangolineage.sh
+++ data/get-sequences-with-pangolineage.sh
@@ -2,7 +2,8 @@
 # cd data
 # ./download.sh
 
-# Unzip (-k means keep original)
+# Unzip 
+# (-k means keep original)
 # Unfortunately, awk and seqtk subseq both need unzipped files
 unxz -k sequences.fasta.xz 
 gunzip -k metadata.tsv.gz
@@ -15,5 +16,6 @@ awk '{ if ($20 != "?") { print $1} }' metadata.tsv | awk 'NR!=1' > metadata_pang
 seqtk subseq sequences.fasta metadata_pango-names.txt > sequences_pangolin.fasta
 
 # Clean up
-xz sequences_pangolin.fasta
-rm data/sequences.fasta
+xz sequences_pangolin.fasta # don't keep original
+rm sequences.fasta
+rm metadata.tsv
