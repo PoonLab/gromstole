7c9e52a5bbda95f31bfeee86ad8936168fe7b943
Art Poon <artpoon@gmail.com>
2021-10-20 17:35:29 -0400

    adding ref file

--- scripts/parse-vcf.R
+++ scripts/parse-vcf.R
@@ -1,57 +1,45 @@
 setwd('~/git/wastewater')
 
-# load VCF from Virontus on Landgraff Nanopore data
-longshot <- read.table("data/results2/vcf/SRR13912812-MN908947.3.longshot.vcf")
-medaka <- read.table("data/results2/vcf/SRR13912812-MN908947.3.medaka.vcf")
+## load VCF from Virontus on Landgraff Nanopore data
+#longshot <- read.table("data/results2/vcf/SRR13912812-MN908947.3.longshot.vcf")
+#medaka <- read.table("data/results2/vcf/SRR13912812-MN908947.3.medaka.vcf")
+#names(longshot) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
+#                     'info', 'format', 'sample')
+#names(medaka) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
+#                     'info', 'format', 'sample')
+#longshot$depth <- sapply(longshot$info, function(x) {
+#  tokens <- strsplit(x, ";")[[1]]
+#  dp <- as.integer(strsplit(tokens[1], "=")[[1]][2])
+#})
+#longshot$count <- sapply(longshot$info, function(x) {
+#  tokens <- strsplit(x, ";")[[1]]
+#  sum(as.integer(strsplit(strsplit(tokens[2], "=")[[1]][2], ",")[[1]]))
+#})
 
-# CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	SAMPLE
-names(longshot) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
-                     'info', 'format', 'sample')
-names(medaka) <- c('chrom', 'pos', 'id', 'ref', 'alt', 'qual', 'filter', 
-                     'info', 'format', 'sample')
-
-
-longshot$depth <- sapply(longshot$info, function(x) {
-  tokens <- strsplit(x, ";")[[1]]
-  dp <- as.integer(strsplit(tokens[1], "=")[[1]][2])
-})
-
-longshot$count <- sapply(longshot$info, function(x) {
-  tokens <- strsplit(x, ";")[[1]]
-  sum(as.integer(strsplit(strsplit(tokens[2], "=")[[1]][2], ",")[[1]]))
-})
-
-# note these are substitutions only
-longshot$rel.freq <- longshot$count / longshot$depth
-
-longshot$label <- paste("~", longshot$pos, substr(longshot$alt, 1, 1), sep="")
+## note these are substitutions only
+#longshot$rel.freq <- longshot$count / longshot$depth
+#longshot$label <- paste("~", longshot$pos, substr(longshot$alt, 1, 1), sep="")
+#index <- match(longshot$pos, medaka$pos)
+#longshot$medaka.q <- medaka$qual[index]
 
 
-index <- match(longshot$pos, medaka$pos)
-longshot$medaka.q <- medaka$qual[index]
+ivar <- read.csv('data/SRR13912812.ivar.tsv', sep='\t', header=T)
 
 # load our results for Illumina data from same study
 ont <- read.csv('data/ontario.csv')
 
-index <- match(longshot$label, ont$label)
+ivar$label <- paste('~', ivar$POS, ivar$ALT, sep='')
 
-longshot$illumina <- ont$frequency[index]
+index <- match(ont$label, ivar$label)
+ont$ivar <- ivar$ALT_FREQ[index]
 
-png(file='temp.png', width=5*150, height=5*150, res=150)
-par(mar=c(5,5,1,1))
-plot(longshot$rel.freq, longshot$illumina, xlim=c(0,1), ylim=c(0,1), type='n')
-text(longshot$rel.freq, longshot$illumina, label=longshot$label, cex=0.5)
-abline(a=0, b=1)
-dev.off()
-
-tab1 <- read.csv('data/landgraff-table1.csv', header=T)
-index <- match(tab1$label, longshot$label)
+#longshot$illumina <- ont$frequency[index]
 
 
 # Our try at Nanopore
 nano <- read.csv('~/git/wastewater/data/SRR13912812.mapped.csv')
 
-# coverage plog
+# coverage plot
 temp <- cbind(nano$position, nano$coverage)
 temp <- temp[!duplicated(temp), ]
 temp <- temp[order(temp[,1]), ]
@@ -62,9 +50,20 @@ dev.off()
 index <- match(ont$label, nano$label)
 ont$nanopore <- nano$frequency[index]
 
-foo <- ont[ont$coverage > 100, ]
-plot(foo$frequency, foo$nanopore)
+res <- 72
+png(filename="~/Desktop/nanopore.png", width=5*res, height=5*res, res=res)
+par(mfrow=c(1,1), mar=c(5,5,1,1))
+plot(ont$nanopore, ont$ivar, xlim=c(0,1), ylim=c(0,1),
+     xlab='UWO Nanopore pipeline', ylab='iVar (Landgraff)')
+abline(a=0, b=1, col=rgb(0,0,0,0.3))
+dev.off()
 
+
+png(filename="~/Desktop/platforms.png", width=5*res, height=5*res, res=res)
+par(mfrow=c(1,1), mar=c(5,5,1,1))
+plot(foo$frequency, foo$nanopore, xlab='UWO Illumina', ylab='UWO Nanopore', col=)
+abline(a=0, b=1, col=rgb(0,0,0,0.3))
+dev.off()
 
 index <- match(longshot$label, nano$label)
 longshot$our.nano <- nano$frequency[index]
