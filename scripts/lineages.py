ee6c63e943624eccda75413bb4947433558ff9ea
DBecker7 <d7becker@gmail.com>
2021-10-22 14:57:04 -0400

    Pull old versions of functions so the code still works

--- scripts/lineages.py
+++ scripts/lineages.py
@@ -13,6 +13,131 @@ import lzma
 from seq_utils import iter_fasta, batch_fasta
 import minimap2
 import sys
+import subprocess
+import re
+import json
+import time
+
+def encode_diffs_classic(row, reflen=29903, alphabet='ACGT'):
+    """
+    Serialize differences of query sequences to reference
+    genome, which comprise nucleotide substitutions, in-frame
+    indels, and locations of missing data.
+    NOTE: runs of 'N's are also represented by 'X' tokens in the CIGAR
+    string.
+    :param row:  tuple from minimap2(), i.e., (qname, rpos, cigar, seq)
+    :param reflen:  length of reference genome
+    """
+    qname, rpos, cigar, seq = row  # unpack tuple
+    diffs = []
+    missing = []
+    if rpos > 0:
+        # incomplete on left
+        missing.append(tuple([0, rpos]))
+    left = 0  # index for query
+
+    tokens = re.findall(r'  (\d+)([MIDNSHPX=])', cigar, re.VERBOSE)
+    for length, operator in tokens:
+        length = int(length)
+        substr = seq[left:(left + length)]
+        if operator == 'X':
+            # each nucleotide is a separate diff
+            if 'N' in substr:
+                # for now, assume the whole substring is bs
+                missing.append(tuple([rpos, rpos+length]))
+            else:
+                # assume adjacent mismatches are independent substitutions
+                for i, nt in enumerate(substr):
+                    if nt in alphabet:
+                        diffs.append(tuple(['~', rpos + i, nt]))
+                    else:
+                        # skip ambiguous base calls, like "R"
+                        missing.append(tuple([rpos+i, rpos+i+1]))
+
+            left += length
+            rpos += length
+        elif operator == 'S':
+            # discard soft clip
+            left += length
+        elif operator == 'I':
+            # insertion relative to reference
+            diffs.append(tuple(['+', rpos, substr]))
+            left += length
+        elif operator == 'D':
+            # deletion relative to reference
+            diffs.append(tuple(['-', rpos, length]))
+            rpos += length
+        elif operator == '=':
+            # exact match
+            left += length
+            rpos += length
+        elif operator == 'H':
+            # hard clip, do nothing
+            pass
+        else:
+            print("ERROR: unexpected operator {}".format(operator))
+            sys.exit()
+
+    # update missing if sequence incomplete on the right
+    if rpos < reflen:
+        missing.append(tuple([rpos, reflen]))
+
+    return qname, diffs, missing
+
+def minimap2_fasta(infile, ref, stream=False, path='minimap2', nthread=3, minlen=29000):
+    """
+    Wrapper function for minimap2.
+
+    :param infile:  file object or StringIO
+    :param ref:  str, path to FASTA with reference sequence(s)
+    :param stream:  bool, if True then stream data from <infile> object via stdin
+    :param path:  str, path to binary executable
+    :param nthread:  int, number of threads for parallel execution of minimap2
+    :param minlen:  int, filter genomes below minimum length; to accept all, set to 0.
+
+    :yield:  query sequence name, reference index, CIGAR and original
+             sequence
+    """
+    if stream:
+        # input from StringIO in memory
+        p = subprocess.Popen(
+            [path, '-t', str(nthread), '-a', '--eqx', ref, '-'], encoding='utf8',
+            stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
+        )
+        output, outerr = p.communicate(infile)
+        output = output.split('\n')
+    else:
+        # input read from file
+        p = subprocess.Popen(
+            [path, '-t', str(nthread), '-a', '--eqx', ref, infile.name],
+            stdout=subprocess.PIPE, stderr=subprocess.DEVNULL
+        )
+        output = map(lambda x: x.decode('utf-8'), p.stdout)
+
+    print(len(output))
+    for line in output:
+        if line == '' or line.startswith('@'):
+            # split on \n leaves empty line; @ prefix header lines
+            continue
+        
+        qname, flag, rname, rpos, _, cigar, _, _, _, seq = \
+            line.strip('\n').split('\t')[:10]
+
+        if rname == '*' or ((int(flag) & 0x800) != 0):
+            # did not map, or supplementary alignment
+            continue
+
+        if len(seq) < minlen:
+            # sequence too short
+            continue
+
+        # validate CIGAR string
+        is_valid = re.match(r'^((\d+)([MIDNSHPX=]))*$', cigar)
+        if not is_valid:
+            raise RuntimeError('Invalid CIGAR string: {!r}.'.format(cigar))
+
+        rpos = int(rpos) - 1  # convert to 0-index
+        yield qname, rpos, cigar, seq
 
 # import tile coordinates file
 tiles = []
@@ -32,9 +157,25 @@ handle = lzma.open("data/sequences.fasta.xz", 'rt')
 ref_file = "data/NC_045512.fa"
 
 batcher = batch_fasta(filter(lambda x: x[0] in lineages, iter_fasta(handle)), size=100)
+counter = 0
+res = []
+tic0 = time.perf_counter()
 for fasta in batcher:
-    mm2 = minimap2.minimap2(fasta, ref=ref_file, stream=True, nthread=1, minlen=29000)
-    for qname, diffs, missing in [minimap2.encode_diffs(row) for row in mm2]:
-        print(qname, diffs, missing)
+    tic = time.perf_counter()
+    mm2 = minimap2_fasta(fasta, ref=ref_file, stream=True, nthread=4, minlen=28500)
+    counter += 1
+    for qname, diffs, missing in [encode_diffs_classic(row) for row in mm2]:
+        diffs = ["".join(map(str, x)) for x in diffs]
+        res.append({"qname": qname, "lineage": lineages[qname], "diffs": diffs, "missing": missing})
         break
-    break
+    if not counter % 20:
+        toc = time.perf_counter()
+        print(f"{toc - tic:0.3f} for loop {counter}, {toc-tic0:0.3f} total.")
+    #if counter > 10:
+    #    break
+
+toc0 = time.perf_counter()
+print(f"total time: {toc0 - tic0:0.3f}")
+
+serial = json.dumps(res).replace('},', '},\n')
+open("data/pangodiffs.json", "w").write(serial)
