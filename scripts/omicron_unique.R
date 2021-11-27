8bb3c9676146f411a62953280d25caeb5f806ede
DBecker7 <d7becker@gmail.com>
2021-11-27 14:36:40 -0500

    Better output format (incl. freq) for #18

--- scripts/omicron_unique.R
+++ scripts/omicron_unique.R
@@ -10,15 +10,18 @@ oh_diffs <- sapply(1:nrow(ohmy), function(x) paste(ohmy[x, 1:3], collapse = ""))
 pd <- read.csv(here("data", "pangodiffs.csv"))
 pango_diffs <- strsplit(pd$diffs, split = "-")
 
-# Initialize list
-# oh_distinct[["0"]]: any omicron mutation that appears in NONE of the pangolin sequences
-# oh_distinct[["1"]]: omicron mutations that only appear in ONE of the other sequences
-oh_distinct <- list()
+# Initialize dataframe
+# pango_matches is the number of different sequences from pangodiffs
+# that particular mutation appears in.
+oh_distinct <- data.frame(mutation = oh_diffs, freq = ohmy$freq,
+    pango_matches = 0)
 
 for(i in oh_diffs){
     pango_match <- sapply(pango_diffs, function(x) i %in% x)
     count_of_pango_matches <- as.character(sum(pango_match))
-    oh_distinct[[count_of_pango_matches]] <- c(oh_distinct[[count_of_pango_matches]], i)
+    oh_distinct$pango_matches[oh_distinct$mutation == i] <- count_of_pango_matches
 }
 
-oh_distinct[order(as.numeric(names(oh_distinct)))]
+oh_distinct
+
+write.csv(oh_distinct, file = here("data", "omicron_v_pango.csv"))
