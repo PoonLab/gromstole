9a4839512a9a4465b26f5cced5064c98e25b263b
DBecker7 <d7becker@gmail.com>
2021-12-06 12:42:10 -0500

    Ignore more things, use correct data in my plots

--- reports/make_ggplot_omicron_muts.R
+++ reports/make_ggplot_omicron_muts.R
@@ -2,6 +2,7 @@ library(here)
 library(tidyr)
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
 cvr <- read.csv(here("results/cvr.csv"), stringsAsFactors = FALSE)
 
@@ -11,7 +12,6 @@ cvr$X <- sapply(strsplit(cvr$X, split = "/"), function(x) {
 cvr <- rename(cvr, sample = X)
 
 cvr_long <- pivot_longer(cvr, -sample, names_to = "mutation", values_to = "coverage")
-head(cvr_long)
 
 ggplot(cvr_long, aes(x = mutation, y = sample, fill = log(coverage))) +
   geom_tile() +
@@ -30,10 +30,21 @@ str(counts)
 
 counts_long <- counts %>%
   select(-X) %>% 
-  pivot_longer(-c("sample", "lab", "coldate", "site"), names_to = "mutation", values_to = "count")
+  pivot_longer(-c("sample", "lab", "coldate", "site"), 
+    names_to = "mutation", values_to = "count") 
 
 ggplot(counts_long, aes(x = mutation, y = sample, fill = log(count))) +
   geom_tile() +
   scale_fill_viridis_c() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-  
+
+
+frequency <- left_join(counts_long, cvr_long, by = c("sample", "mutation")) %>%
+  mutate(frequency = ifelse((!is.na(count)) & coverage > 0, count / coverage, 
+    ifelse(coverage > 0, 0, NA)))
+dim(frequency)
+
+ggplot(frequency, aes(x = mutation, y = sample, fill = log(frequency))) +
+  geom_tile() +
+  scale_fill_viridis_c() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
