9a6ac60b2e97a802079c1cd8bbddb197a7f7562f
ArtPoon <artpoon@gmail.com>
2021-12-13 17:14:32 -0500

    closes #23

--- data/omicron-BA_fixing.R
+++ data/omicron-BA_fixing.R
@@ -1,5 +1,5 @@
 # Fixing Omicron data
-
+setwd('~/git/gromstole')
 omicron <- read.csv("https://github.com/cov-lineages/pango-designation/files/7668225/Omicron_BA.1_BA.2_mutations.csv",
     stringsAsFactors = FALSE)
 names(omicron)[c(1,4)] <- c("mut_nuc", "mut_aa")
