8ae264d21da8dcf81bb0faa1f4d596d34e4376cf
DBecker7 <d7becker@gmail.com>
2021-12-21 15:21:56 -0500

    Generalize cocovoc_mcmc.R to all labs/samples

--- scripts/cocovoc_mcmc.R
+++ scripts/cocovoc_mcmc.R
@@ -6,18 +6,6 @@ library(dplyr)
 library(here)
 library(ggridges)
 
-variantmat <- read.csv(here("data", "variantmatrix_med.csv"), row.names = 1)
-variantmat <- as.matrix(variantmat)
-dim(variantmat)
-rankMatrix(variantmat) # Not full rank - some variants will be unidentifiable
-
-cocovoc <- read.csv(here("data", "cocovoc_med.csv"), stringsAsFactors = FALSE)
-
-cocovoc1 <- filter(cocovoc, sample == sample[1], lab == lab[1])
-variantmat1 <- variantmat[, which(!is.na(cocovoc1$coverage2))]
-cocovoc1 <- cocovoc1[!is.na(cocovoc1$coverage2), ]
-cocovoc1$count <- round(cocovoc1$count)
-
 p_model <- "
 model{
     for(i in 1:N) {
@@ -33,50 +21,53 @@ model{
 }
 "
 
-t0 <- Sys.time()
-coda1 <- jags.model(file = textConnection(p_model),
-    data = list(count = cocovoc1$count, N = nrow(cocovoc1),
-        coverage = cocovoc1$coverage2 + 1, # DANGER: Currently a hack to make it run
-        P = nrow(variantmat), variantmat = variantmat),
-    n.adapt = 500, n.chains = 3) %>%
-    coda.samples(n.iter = 2000, variable.names = c("p"))
-difftime(Sys.time(), t0, units = "mins")
+variantmat <- read.csv(here("data", "variantmatrix_med.csv"), row.names = 1)
+variantmat <- as.matrix(variantmat)
+dim(variantmat)
+rankMatrix(variantmat) # Not full rank - some variants will be unidentifiable
 
-summary(coda1)
+cocovoc <- read.csv(here("data", "cocovoc_med.csv"), stringsAsFactors = FALSE)
 
-codf <- bind_rows(lapply(1:length(coda1), 
-    function(i) {
-        x <- as.data.frame(coda1[[i]])
-        x$chain <- i
-        x$sample <- 1:nrow(x)
-        x
+labs <- unique(cocovoc$lab)
+for(lab in labs) {
+    samples <- unique(cocovoc[cocovoc$lab == lab])
+
+    for(sample in samples){
+        cocovoc1 <- filter(cocovoc, sample == sample, lab == lab)
+        variantmat1 <- variantmat[, which(!is.na(cocovoc1$coverage2))]
+        cocovoc1 <- cocovoc1[!is.na(cocovoc1$coverage2), ]
+        cocovoc1$count <- round(cocovoc1$count)
+
+        
+
+        t0 <- Sys.time()
+        coda1 <- jags.model(file = textConnection(p_model),
+            data = list(count = cocovoc1$count, N = nrow(cocovoc1),
+                coverage = cocovoc1$coverage2 + 1, # DANGER: Currently a hack to make it run
+                P = nrow(variantmat), variantmat = variantmat),
+            n.adapt = 500, n.chains = 2) %>%
+            coda.samples(n.iter = 3000, variable.names = c("p"))
+        print(difftime(Sys.time(), t0, units = "mins"))
+
+        codf <- bind_rows(lapply(1:length(coda1), 
+            function(i) {
+                x <- as.data.frame(coda1[[i]])
+                x$chain <- i
+                x$sample <- 1:nrow(x)
+                x
+            }
+        ))
+        voc_match <- data.frame(name = paste0("p.", 1:nrow(variantmat), "."), voc = rownames(variantmat))
+        codf_long <- tidyr::pivot_longer(codf, -c("chain", "sample")) %>%
+            left_join(voc_match, by = "name")
+
+        write.csv(
+            x = codf_long, 
+            file = here("results", paste0(lab, "-",sample, "-", "cocovoccoda.csv")), 
+            row.names = FALSE
+        )
+
     }
-))
+}
 
-write.csv(codf, file = here("results", "cocovoccoda.csv"), row.names = FALSE)
 
-voc_match <- data.frame(name = paste0("p.", 1:nrow(variantmat), "."), voc = rownames(variantmat))
-
-codf_long <- tidyr::pivot_longer(codf, -c("chain", "sample")) %>%
-    left_join(voc_match, by = "name")
-
-ggplot(codf_long, aes(x = value, colour = voc)) + 
-    geom_density() +
-    xlim(c(0.005, 0.4))
-
-codf_long %>%
-    select(-chain, -sample, -name) %>%
-    group_by(voc) %>%
-    summarise(median = median(value)) %>%
-    ggplot() +
-        aes(y = median, x = voc, fill = voc) +
-        theme_bw() +
-        geom_col() + 
-        theme(legend.position = "none") +
-        coord_flip()
-
-ggplot(codf_long) + 
-    theme_bw() +
-    aes(y = voc, x = value, colour = chain) +
-    geom_density_ridges(scale = 2) +
-    theme(legend.position = "none")
