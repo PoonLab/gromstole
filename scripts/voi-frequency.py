793d74aeae6630f491958d2d1849537379235f6b
DBecker7 <d7becker@gmail.com>
2022-01-11 09:38:21 -0500

    Remove duplicate mutations

--- scripts/voi-frequency.py
+++ scripts/voi-frequency.py
@@ -14,16 +14,21 @@ args = parser.parse_args()
 with gzip.open("data/count-mutations_nsgb.json.gz", 'rt') as handle:
     mutations = json.load(handle)
 
+# Record list of variants of interest
 #variant_list = ["B.1.1.529", "BA.1", "BA.2"]
 variant_list = args.variant_list
 for key in variant_list:
     if key not in mutations.keys():
         sys.exit("Error: " + key + " is not in nsgb mutations.")
 
+# Gather all mutations from all VoIs.
 variant = []
 for key in variant_list:
     variant.extend(mutations[key]['mutations'])
+variant = list(set(variant)) # remove duplicates
+variant.sort()
 
+# Get the frequency of VoI mutations for all sequences incl. VoIs).
 outfile = open("data/voi-frequency_" + "_".join(variant_list).replace(".", "-") + ".csv", 'w')
 outfile.write("lineage,variant_of_interest,count," + ",".join(variant) + '\n')
 for lineage, ldata in mutations.items():
