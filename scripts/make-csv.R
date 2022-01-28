fbf162676c9270a67587afcd120e2cbc059d6ebb
Art Poon <artpoon@gmail.com>
2022-01-28 14:38:59 -0500

    handling edge cases in make-csv.R

--- scripts/make-csv.R
+++ scripts/make-csv.R
@@ -2,31 +2,44 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) != 2) {
   stop("\n\nUsage: Rscript make-csv.R [input JSON] [output CSV]\n",
        "  input JSON:  JSON produced by estimate-freqs.R\n",
-       "  output PDF:  path to write CSV\n\n")
+       "  output CSV:  path to write CSV\n\n")
 }
 require(jsonlite)
 input <- jsonlite::read_json(args[1], simplifyVector = TRUE)
 outfile <- args[2]
 
-# date collected, site, sequencing platform, number of mutations, frequency range, interpretation
-counts <- input$counts[names(input$coverage)]
-fq <- counts/input$coverage
-fq[input$coverage<10] <- NA
+# deal with columns of all NAs in counts
+coverage <- input$coverage[names(input$counts)]
+
+# deal with missing metadata
+coldate <- input$metadata$coldate
+if (is.null(coldate)) {
+  coldate <- NA
+}
+site <- input$metadata$site
+if (is.null(site)) {
+  site <- NA
+}
+
+fq <- input$counts / coverage
+fq[coverage<10] <- NA
 
 df <- data.frame(
-  coldate=input$metadata$coldate,
-  site=input$metadata$site,
-  platform="Illumina",
-  n.muts=apply(counts, 1, function(x) sum(x>0, na.rm=T)),
-  cover=apply(input$coverage, 1, function(x) sum(x>0)),
-  min.freq=apply(fq, 1, function(x) 100*min(x, na.rm=T)),
-  med.freq=apply(fq, 1, function(x) 100*median(x, na.rm=T)),
-  max.freq=apply(fq, 1, function(x) 100*max(x, na.rm=T))
+  coldate=coldate,
+  site=site,
+  n.muts=apply(input$counts, 1, function(x) sum(x>0, na.rm=T)),
+  cover=apply(coverage, 1, function(x) sum(x>0)),
+  #min.freq=apply(fq, 1, function(x) 100*min(x, na.rm=T)),
+  #med.freq=apply(fq, 1, function(x) 100*median(x, na.rm=T)),
+  #max.freq=apply(fq, 1, function(x) 100*max(x, na.rm=T))
+  est=100*input$estimate$est,
+  lower.95=ifelse(is.null(input$estimate$lower.95), NA, 100*input$estimate$lower.95),
+  upper.95=ifelse(is.null(input$estimate$upper.95), NA, 100*input$estimate$upper.95)
 )
 
-write.table(t(c("", "Date collected", "Site", "Sequencing platform",
+write.table(t(c("", "Date collected", "Site", 
             "Positions with mutations detected", "Positions covered",
-            "Minimum frequency (%)", "Median frequency (%)", 
-            "Maximum frequency (%)")), outfile, sep=',', 
+            "Estimated frequency (%)", "Lower 95% CI (%)", 
+            "Upper 95% CI (%)")), outfile, sep=',', 
             quote=TRUE, col.names=FALSE, row.names=F)
 write.table(df, outfile, sep=',', quote=T, col.names=FALSE, append=TRUE)
